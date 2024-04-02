299f0fbdd59976bade8c0e2d016dc8941deccce7
Tejas Tanna <tannatejas@gmail.com>
2024-04-02 20:59:27 +0100

    initial commit

---
+++ src/test/java/com/tt/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
